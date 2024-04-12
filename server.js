1de6b750b44e5cb8f2f4e5d44a9299860a49ee84
YaoChen19 <99479155+YaoChen19@users.noreply.github.com>
2024-04-12 20:26:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
 
-const iotHubConnectionString = process.env.IotHubConnectionString;
+const iotHubConnectionString = "HostName=YaoHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=Fvt8gtfAK8A8v6IoLB4J03VqAnhamlm0hAIoTC1Qers=";
 if (!iotHubConnectionString) {
   console.error(`Environment variable IotHubConnectionString must be specified.`);
   return;
 }
 console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
 
-const eventHubConsumerGroup = process.env.EventHubConsumerGroup;
+const eventHubConsumerGroup = "webconsumer";
 console.log(eventHubConsumerGroup);
 if (!eventHubConsumerGroup) {
   console.error(`Environment variable EventHubConsumerGroup must be specified.`);
